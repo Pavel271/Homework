4aeb2479ae2b3b554f0c8d7efdde808c19079fc4
Pavel271 <zagretdinovpasha@gmail.com>
2024-07-26 14:37:46 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,13 @@
+#name = input("Введите имя: ")
+#current_year = 2024
+#date_of_birth = int(input("В каком году Вы родились? "))
+#age = current_year - date_of_birth
+#print("Здравствуйте, ", name)
+#print("В этом году Вам ", age, "лет")
+#print('привет, я строка в нижнем регистре')
+
+my_string = input('Введите текст: ')
+print('Длина строки:', len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
