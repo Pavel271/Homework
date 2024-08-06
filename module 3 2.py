5d96524c6b9dfaf1e935ab25e94d2811443c38eb
Pavel271 <zagretdinovpasha@gmail.com>
2024-08-06 14:07:05 +0300

    Add files via upload

---
+++ module 3 2.py
@@ -0,0 +1,21 @@
+# def print_params(a,*, b, c):
+#     print(a, b, c)
+#
+#
+# print_params(a=1, b=2, c=6)
+
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+send_email('messagje','university.help@gmail.com','@.com')
+send_email('messagje','university.help@gmail.com')
+send_email('messagje','universi@.com')
+send_email('messagje','universi@.co')
+send_email('messagje','universi.com')
