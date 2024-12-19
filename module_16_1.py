4542bc6959fbf780aa63c25b5e5d220dd8c97b1a
Pavel271 <zagretdinovpasha@gmail.com>
2024-12-19 13:34:25 +0000

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get('/')
-async def main_page() -> dict:
-    return {'message': 'Главная страница'}
+async def main_page() -> str:
+    return 'Главная страница'
 
 @app.get('/user/admin')
-async def admin_page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def admin_page() -> str:
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def user_page(user_id: int) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def user_page(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
-async def user_info(username: str = Query(...), age: int = Query(...)) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def user_info(username: str = Query(...), age: int = Query(...)) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
