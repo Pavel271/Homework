e3d3f4961a1b86827e1862d485147a3ef37f3113
Pavel271 <zagretdinovpasha@gmail.com>
2025-01-01 12:25:33 +0000

    Add files via upload

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,10 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+from app.routers.schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.backend.db import Base
@@ -15,3 +22,53 @@ class User(Base):
 
 from sqlalchemy.schema import CreateTable
 print(CreateTable(User.__table__))
+
+router = APIRouter(prefix='/user', tags=['user'])
+
+@router.get('/', response_model=User)
+async def all_users(db: Session = Depends(get_db)):
+    users = db.execute(select(User)).scalars().all()
+    return users
+
+@router.get('/{user_id}', response_model=User)
+async def user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    return user
+
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+async def create_user(user: CreateUser, db: Session = Depends(get_db)):
+    existing_user = db.execute(select(User).where(User.username == user.username)).scalar_one_or_none()
+    if existing_user:
+        raise HTTPException(status_code=400, detail='Пользователь с таким именем существует')
+    new_user = User(
+        username=user.username,
+        firstname=user.firstname,
+        lastname=user.lastname,
+        age=user.age,
+        slug=slugify(user.username))
+    db.execute(insert(User).values(new_user))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
+@router.put('/update/{user_id}', status_code=status.HTTP_200_OK)
+async def update_user(user_id: int, user: UpdateUser, db: Session = Depends(get_db)):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    db.execute(update(User).where(User.id == user_id).values(
+        firstname=user.firstname,
+        lastname=user.lastname,
+        age=user.age))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'Пользователь обновлён'}
+
+@router.delete("/delete/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail='Пользователь не найден')
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_204_NO_CONTENT, 'transaction': 'Пользователь обновлён'}
